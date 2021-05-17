85a0960473790d7e09341e0be4366347feacade5
Biprajit Majumder <biprajit163@gmail.com>
2021-05-17 13:20:59 -0400

    set messageArr and nounsArr

--- game.js
+++ game.js
@@ -2,16 +2,18 @@
 const fs = require('fs');
 
 const ufo_data = require('./data/ufo.js');
-const ufo_arr = ufo_data.ufo_arr;
+const ufoArr = ufo_data.ufo_arr;
 
 const message_data = fs.readFileSync('./data/messages.txt', 'utf8');
-
+const messageArr = message_data.split('\n'); 
 
 const nouns_data = fs.readFileSync('./data/nouns.txt', 'utf8');
 const nouns_arr = nouns_data.split('\n');
 
-console.log(nouns_arr);
 
+console.log(ufoArr);
+console.log(messageArr);
+console.log(nouns_arr);
 
 
 
