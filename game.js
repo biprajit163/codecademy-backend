85601653e3a940be2203d4acc68880cd570fea87
Biprajit Majumder <biprajit163@gmail.com>
2021-05-17 13:21:34 -0400

    nounsArr

--- game.js
+++ game.js
@@ -8,12 +8,12 @@ const message_data = fs.readFileSync('./data/messages.txt', 'utf8');
 const messageArr = message_data.split('\n'); 
 
 const nouns_data = fs.readFileSync('./data/nouns.txt', 'utf8');
-const nouns_arr = nouns_data.split('\n');
+const nounsArr = nouns_data.split('\n');
 
 
 console.log(ufoArr);
 console.log(messageArr);
-console.log(nouns_arr);
+console.log(nounsArr);
 
 
 
