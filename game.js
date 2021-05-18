61c0d73c4833a01db247c97a5792d8d5f5638429
Biprajit Majumder <biprajit163@gmail.com>
2021-05-18 17:18:06 -0400

    user input to console

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
+"use strict"
+
 
 const fs = require('fs');
+const ps = require('prompt-sync')({ sigint: true });
+
 
 const ufo_data = require('./data/ufo.js');
 const ufoArr = ufo_data.ufo_arr;
@@ -11,12 +15,35 @@ const nouns_data = fs.readFileSync('./data/nouns.txt', 'utf8');
 const nounsArr = nouns_data.split('\n');
 
 
-console.log(ufoArr);
-console.log(messageArr);
-console.log(nounsArr);
 
 
+const playGame = () => {
+    
+    let isPlaying = true;
+
+    while(isPlaying === true) {
+
+        // Game logic goes here
+        console.log('Game is Running');
+
+        endGame(isPlaying);
+    }
+};
 
 
+const endGame = (isPlaying) => {
+    let userAns = ps('Do you want to keep playing? [yes or no] : ');
 
+    if(userAns.toLowerCase() === 'yes') {
+        isPlaying = true;
+    } else if(userAns.toLowerCase() === 'no') {
+        process.exit();
+    } else {
+        console.log('That is not a valid answer please type "yes" or "no"');
+    }
+
+    return isPlaying;
+}
+
+playGame();
 
