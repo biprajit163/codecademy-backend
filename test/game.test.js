ab04fb7a9ec50fad2fb7ad2330af14e0a39574f4
Biprajit Majumder <biprajit163@gmail.com>
2021-05-19 23:55:42 -0400

    test working for correct and incorrect input

--- test/game.test.js
+++ test/game.test.js
@@ -3,11 +3,20 @@ const assert = require('chai').assert;
 const { testUser } = require('../game');
 
 
-describe('Array', function() {
-    describe('correct letter', function() {
-        it('return true if letter is inside codeword', function() {
+describe('Array', () => {
+    describe('correct letter', () => {
+        it('return true if letter is inside codeword', () => {
             let usrAns = testUser();
             assert.equal(usrAns, true);
+            assert.typeOf(usrAns, 'boolean');
+        });
+    });
+
+    describe('incorrect letter', () => {
+        it('return false if letter is not inside codeword', () => {
+            let usrAns = testUser();
+            assert.equal(usrAns, false);
+            assert.typeOf(usrAns, 'boolean');
         });
     });
 });
