23f1ba554c67b6754f073d3ddaaeab9193f276de
Biprajit Majumder <biprajit163@gmail.com>
2021-05-23 20:54:43 -0400

    encouraging message

--- game.js
+++ game.js
@@ -9,8 +9,8 @@ const ufoArr = ufo_data.ufo_arr;
 const message_data = fs.readFileSync("./data/messages.txt", "utf8");
 const messageArr = message_data.split("\n");
 
-// Pick a random codeword from the nouns Arr;
 
+// Pick a random codeword from the nouns Arr;
 const nouns_data = fs.readFileSync("./data/nouns.txt", "utf8");
 const nounsArr = nouns_data.split("\n");
 let codeword = nounsArr[Math.floor(Math.random() * nounsArr.length)];
@@ -74,8 +74,6 @@ const playGame = () => {
       }
 
       console.log(codeword);
-      // console.log("correct guesses: " + correctGuesses);
-      // console.log("incorrect guesses: " + incorrectGuesses + "\n");
 
       if (incorrectGuesses.length === 0) {
         console.log("Incorrect Guesses:" + "\n" + "None" + "\n");
@@ -89,9 +87,6 @@ const playGame = () => {
       }
 
       console.log(codeDash.join(" ").toUpperCase());
-
-      nounsMatches = potentialWords(codeDash, codeword);
-      console.log(`Number of dictionary matches: ${nounsMatches}` + "\n");
 
       let checkLetter = true;
 
@@ -125,9 +120,7 @@ const playGame = () => {
           checkLetter = false;
           console.log(ufoArr[ufoLevel]);
           incorrectGuesses.push(userGuess);
-          console.log(
-            "Incorrect! The tractor beam pulls the person in further" + "\n"
-          );
+          console.log(encourageMessage(messageArr));
           ufoLevel++;
         } else if (userGuess.split("").length > 1) {
           console.log(
@@ -152,6 +145,13 @@ const checkWin = (codeword, codeDash) => {
   return result;
 };
 
+
+const encourageMessage = (encourage) => {
+  let randIndex = Math.floor(Math.random() * encourage.length);
+  return encourage[randIndex];
+};
+
+
 const endGame = (isPlaying) => {
   let run = true;
 
@@ -169,42 +169,6 @@ const endGame = (isPlaying) => {
   }
 
   return isPlaying;
-};
-
-
-const potentialWords = (codeDash, codeword, similarity = 0, dashPercent = 0) => {
-  let codewordArr = codeword.split(" ");
-  let nounsIndex = 0;
-  let possibleWords = 0;
-  let codewordNum = 0;
-  let codeDashNum = 0;
-  
-  while(nounsIndex < nounsArr.length) {
-    let letterCount = 0;
-
-    for(let i=0; i < codeDash.length; i++) {
-      if(codeDash[i] !== "_") letterCount++;
-      codeDashNum += codeDash[i].charCodeAt(0);
-    }
-
-    for(let i=0; i < codewordArr.length; i++) {
-      codewordNum += codewordArr[i].charCodeAt(0);
-    }
-
-    dashPercent = letterCount/codeDash.length;
-    similarity = (codeDashNum/codewordNum);
-
-    if(similarity.toFixed(2) >= dashPercent.toFixed(2)) possibleWords++;
-    
-    nounsIndex++;
-  }
-
-  console.log(`codewordNum ${codewordNum}`);
-  console.log(`codeDashNum ${codeDashNum}`);
-
-  console.log(`Dash Percent ${dashPercent.toFixed(2)}`);
-  console.log(`similarity Percent ${similarity.toFixed(2)}`);
-  return possibleWords;
 };
 
 
